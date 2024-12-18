1f7b2d2d0114d37c58ac6dded8bdea6f0752ba1d
DESKTOP-LH27L2V\Nick <nicknick765@gmail.com>
2024-12-19 02:02:01 +0800

    add shortcut on top

--- src/config.ts
+++ src/config.ts
@@ -38,11 +38,11 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Home,
     LinkPreset.Archive,
     LinkPreset.About,
-    // {
-    //   name: 'GitHub',
-    //   url: 'https://github.com/Mojito765',     // Internal links should not include the base path, as it is automatically added
-    //   external: true,                               // Show an external link icon and will open in a new tab
-    // },
+    {
+      name: 'Linktree',
+      url: 'https://linktr.ee/nik765',     // Internal links should not include the base path, as it is automatically added
+      external: true,                               // Show an external link icon and will open in a new tab
+    },
   ],
 }
 
